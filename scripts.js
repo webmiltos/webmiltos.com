1d85ec26caa379b5dac149da57a453f146a4480c
webmiltos <webmiltos@gmail.com>
2024-11-14 11:38:43 +0200

    add localStorage

--- scripts.js
+++ scripts.js
@@ -1,24 +1,45 @@
-$(document).ready(function() {
-   
-    $("#add-button").click(function() {
+$(document).ready(function () {
+    if (localStorage.getItem("shoppingList")) {
+        var savedList = JSON.parse(localStorage.getItem("shoppingList"));
+        savedList.forEach(function (item) {
+            addListItem(item);
+        });
+    }
+
+    $("#add-button").click(function () {
         var newItem = $("#item").val();
         if (newItem.length > 0) {
-            var item = $("<li></li>").text(newItem);
-            var deleteButton = $("<button></button>").text("delete");
-            
-            deleteButton.click(function() {
-                $(this).parent().remove();  // Διαγραφή του li
-            });
-            
-            item.append(deleteButton);
-            $("#shopping-list").append(item);
-            $("#item").val("");  // Καθαρισμός του input
+            addListItem(newItem);
+            saveListToStorage();
+            $("#item").val("");
         }
     });
 
-    $("#item").keyup(function(e) {
+    $("#item").keyup(function (e) {
         if (e.code === "Enter") {
             $("#add-button").click();
         }
     });
+
+    function addListItem(itemText) {
+        var item = $("<li></li>").text(itemText);
+        var deleteButton = $("<button></button>").text("delete");
+
+        deleteButton.click(function () {
+            $(this).parent().remove();
+            saveListToStorage();
+        });
+
+        item.append(deleteButton);
+        $("#shopping-list").append(item);
+    }
+
+    function saveListToStorage() {
+        var listItems = [];
+        $("#shopping-list li").each(function () {
+            listItems.push($(this).text().replace('delete', '').trim());
+        });
+        localStorage.setItem("shoppingList", JSON.stringify(listItems));
+    }
 });
+
